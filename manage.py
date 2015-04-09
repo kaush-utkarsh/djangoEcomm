c7d057c23d0c29a62d32d2862a7a9c2aa739a18a
Harshit Anand <hanand@algoscale.com>
2015-04-09 14:20:01 +0530

    edited login template of registration package

--- manage.py
+++ manage.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nogpo.settings")
-=======
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nogpo_ecommerce.settings")
->>>>>>> e851fdc458cdc922de5550cc9e4e389c0761e76c
-
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
