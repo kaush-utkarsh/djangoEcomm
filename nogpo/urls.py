258329de9e02a3e0903c7e0246b887d0a90fdab4
Harshit Anand <hanand@algoscale.com>
2015-04-17 03:01:21 +0530

    Added static files to project and integrated frontend

--- nogpo/urls.py
+++ nogpo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from buyers import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls')),
+    url(r'^$', views.home, name="home_page" )
 )
